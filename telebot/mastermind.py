5ffcfe8ec53508ee36ad6f93ba72e7cd5e78fc26
rushabh1410 <shahrushabh14101996@gmail.com>
2020-05-10 18:38:52 -0400

    fourth

--- telebot/mastermind.py
+++ telebot/mastermind.py
@@ -1,29 +1,13 @@
-import json
-import datetime
 import requests
 from bs4 import BeautifulSoup
 
-def parsePrice(fname):
-    x=getshort(fname)
-    if x == "" :
-        return "0"
-    url = 'https://finance.yahoo.com/quote/'+x
-    r= requests.get(url)
-    data=r.text
-    soup=BeautifulSoup(data,features="lxml")
-    price = soup.find('div',{'class': 'My(6px) Pos(r) smartphone_Mt(6px)'}).find('span').text
-    return price
 
-def getshort(fname):
+def get_response(msg):
     y=""
-    url = 'https://finance.yahoo.com/quote/'+fname
+    url = 'https://finance.yahoo.com/quote/'+msg
     r= requests.get(url)
     data=r.text
     soup=BeautifulSoup(data,features="lxml")
     if soup.find('td',{'class' : 'data-col0 Ta(start) Pstart(6px) Pend(15px)'}) is not None:
         y = soup.find('td',{'class' : 'data-col0 Ta(start) Pstart(6px) Pend(15px)'}).text
     return y
-
-def get_response(msg):
-    z=parsePrice(msg)
-    return z
