1883ebfae529759a0f271f4b8715e06e22f58620
rushabh1410 <shahrushabh14101996@gmail.com>
2020-05-10 18:44:28 -0400

    fourth

--- telebot/mastermind.py
+++ telebot/mastermind.py
@@ -3,11 +3,4 @@ from bs4 import BeautifulSoup
 
 
 def get_response(msg):
-    y=""
-    url = 'https://finance.yahoo.com/quote/'+msg
-    r= requests.get(url)
-    data=r.text
-    soup=BeautifulSoup(data,features="lxml")
-    if soup.find('td',{'class' : 'data-col0 Ta(start) Pstart(6px) Pend(15px)'}) is not None:
-        y = soup.find('td',{'class' : 'data-col0 Ta(start) Pstart(6px) Pend(15px)'}).text
-    return y
+    return "chutiya"
